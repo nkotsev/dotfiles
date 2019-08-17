229a33d4efda0f83715f58cd05152ea1c8158616
Nikolay Kotzev <nikolay.kotzev@MacBook-Pro.local>
2019-08-17 23:34:33 +0300

    Add aliases file

--- .zshrc
+++ .zshrc
@@ -107,3 +107,5 @@ plugins=(
   rbenv
   ruby
 )
+
+source ~/.aliases
---
+++ aliases.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias aliases='vim ~/.aliases'
+alias vimrc='vim ~/.vimrc'
+alias zshrc='vim ~/.zshrc'
+alias resource='source ~/.zshrc'
