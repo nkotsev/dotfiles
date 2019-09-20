af1c14a5e05242856341d122287d77d4ed1d6a52
Nikolay Kotsev <nikolay.kotzev@fundingcircle.com>
2019-09-20 10:27:33 +0300

    Add fd function.

--- aliases.sh
+++ aliases.sh
@@ -15,10 +15,19 @@ alias beke='bundle exec rake'
 alias pyma='python3 manage.py'
 
 # Functions
+# fbr - fuzzy branch change
 fbr() {
   local branches branch
   branches=$(git branch --all | grep -v HEAD) &&
   branch=$(echo "$branches" |
            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# fd - fuzzy cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
 }
