dc386f7b87910ce541736bcac0a8573b03188dc8
Nikolay Kotsev <nikolay.kotzev@fundingcircle.com>
2020-02-27 11:20:54 +0200

    Add rebel-readline for clojure projects.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,11 @@
 {:repl {
   :repl-options {
-    :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}}
- :user {:dependencies [[slamhound "1.5.5"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+    :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl
+                                             :address "localhost"
+                                             :port 55555
+                                             :name "lein"})}}
+ :user {:dependencies [[slamhound "1.5.5"]
+                       [com.bhauman/rebel-readline "0.1.4"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]
+                  "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}
         :plugins [[jonase/eastwood "0.3.7"]]}}
